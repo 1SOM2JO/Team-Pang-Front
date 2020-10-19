9755efaf7ed84f904db3b60e951b25330c55752d
zero3062 <esung246@gmail.com>
2020-10-19 09:32:21 +0900

    [ADD] 메인페이지 추가
    메인페이지 추가 및 이미지 첨가

---
+++ pangfront/src/components/MainPage/MainPage.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import * as S from './styles';
+
+const MainPage = () => {
+    const develope = ['신두평','박재훈','유명철','이성진','이지수','최승교'];
+
+    const developeList = develope.map(
+        dev => (
+            <S.Develope>{dev}</S.Develope>
+        )
+    )
+
+    return (
+        <>
+            <S.MainImage></S.MainImage>
+            <S.Contents1>
+                <S.Text1>
+                    <S.Opacity></S.Opacity>
+                    <S.Content1Text1>농민, 소비자 모두에게 이득되는 거래</S.Content1Text1>
+                    <S.Content1Text2>소비자는 좋은 가격에 질 좋은 상품을 구매, 농민은 그 동안 팔기 힘들었던 상품 판매</S.Content1Text2>
+                    <S.Button>상품 구매</S.Button>
+                </S.Text1>
+                <S.Image1></S.Image1>
+            </S.Contents1>
+            <S.Contents2>
+                <S.Image2></S.Image2>
+                <S.Text2>
+                    <S.Content2Text1>갑갑한 도시에서 벗어나 가족들과 즐거운 체험</S.Content2Text1>
+                    <S.Content2Text2>소비자는 좋은 가격에 질 좋은 상품을 구매, 농민은 그 동안 팔기 힘들었던 상품 판매</S.Content2Text2>
+                    <S.Button>체험하기</S.Button>
+                </S.Text2>
+            </S.Contents2>
+            <S.Bottom>
+                <S.BottomHeader>PANG</S.BottomHeader>
+                {developeList}
+            </S.Bottom>
+        </>
+    )
+}
+
+export default MainPage;
