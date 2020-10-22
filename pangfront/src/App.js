0cabd362053ae8d18098c69e89d7153a771bd3bd
parkjaehoon <yukihoon12@naver.com>
2020-10-22 15:06:10 +0900

    [ADD] 라우팅 기능 추가
    라우팅 추가

--- pangfront/src/App.js
+++ pangfront/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header'
 
 function App() {
   return (
-   <Header>
-     
-   </Header>
+    <Switch>
+      <Route path="/" exact component={Header}></Route>
+{/* 이런식으로 라우팅해서 사용할 것 */}
+    </Switch>
+   
   );
 }
 
