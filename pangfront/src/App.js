e24ea7214b75eaf022090adbba115ce7f4c5f028
ljsoo123 <ljsoo123456@naver.com>
2020-11-06 20:26:12 +0900

    [MODIFY] 라우팅

--- pangfront/src/App.js
+++ pangfront/src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import ApplyItem from './components/DetailItem/applyItem/applyItem';
-import ApplyExperience from './components/DetailItem/apllyExperience/ApplyExperience.js'
-import ViewItem from './components/DetailItem/viewItem/ViewItem.js'
-import Experience from './components/Experience/Experience';
-import Buy from './components/buy/Buy';
-import Login from './components/Login/Login';
-import Register from './components/register/Register';
-import MainPage from './components/MainPage/MainPage';
-import Mypage from './components/MyPage/MyPage';
-import EnterSomething from './components/EnterSomething/EnterSomething';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import ApplyItem from "./components/DetailItem/applyItem/applyItem";
+import ApplyExperience from "./components/DetailItem/apllyExperience/ApplyExperience.js";
+import ViewItem from "./components/DetailItem/viewItem/ViewItem.js";
+import Experience from "./components/Experience/Experience";
+import Buy from "./components/buy/Buy";
+import Login from "./components/Login/Login";
+import Register from "./components/register/Register";
+import MainPage from "./components/MainPage/MainPage";
+import Mypage from "./components/MyPage/MyPage";
+import EnterSomething from "./components/EnterSomething/EnterSomething";
+import RegisterEnter from "./components/registerEnter/RegisterEnter";
 
 function App() {
   return (
@@ -21,12 +22,11 @@ function App() {
       <Route path="/buy" exact component={Buy}></Route>
       <Route path="/login" exact component={Login}></Route>
       <Route path="/register" exact component={Register}></Route>
-      <Route path="/mainPage" exact component={MainPage}></Route>
+      <Route path="/" exact component={MainPage}></Route>
       <Route path="/mypage" exact component={Mypage}></Route>
       <Route path="/EnterSomething" exact component={EnterSomething}></Route>
+      <Route path="/registerenter" exact component={RegisterEnter}></Route>
     </Switch>
-
-
   );
 }
 
